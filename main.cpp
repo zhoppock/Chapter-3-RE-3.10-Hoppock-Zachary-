7fd5c1ff6fba1bf295d8444f5742b6fcd9771c54
Zachary Hoppock <61926087+zhoppock@users.noreply.github.com>
2022-06-27 02:35:06 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,19 @@
+// Hoppock, Zachary
+// doubleEqualsOperator.cpp
+// 9/27/20
+// Testing the == operator
+// Version # 1
+#include <iostream>
+
+using namespace std;
+
+int main()
+{  
+
+count == 0;
+
+if (count < 1)
+  cout << "Less than one.";
+
+return 0;
+}
